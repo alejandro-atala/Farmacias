270b3b1cb1921686035f724378f537f64a50959d
alejandro-atala <alejandro.atala@municipiodetandil.edu.ar>
2023-07-24 19:52:58 -0300

    segundo

--- script.js
+++ script.js
@@ -25,36 +25,7 @@ const farmacias = [
   // Llamar a la función para mostrar la farmacia de turno al cargar la página
   mostrarFarmaciaDeTurno();
   
-  // Aquí puedes agregar el código para integrar Google AdMob y mostrar los anuncios en el elemento "anuncio"
-  // Ten en cuenta que necesitarás el SDK de AdMob y tu ID de anuncio para hacerlo correctamente.
-  // Dentro de tu archivo script.js
 
-// Configurar el anuncio de AdMob
-function configurarAnuncio() {
-    // Reemplaza 'TU_ID_ADMOB' con el ID de tu anuncio obtenido en AdMob
-    const adUnitID = 'ca-app-pub-9516551446751407~8397283315';
-    
-    // Crea el anuncio
-    const adsbygoogle = window.adsbygoogle || [];
-    adsbygoogle.push({ google_ad_client: adUnitID });
-  }
-  
-  // Mostrar el anuncio
-  function mostrarAnuncio() {
-    // Reemplaza 'anuncio-container' con el ID del contenedor que creaste
-    const container = document.getElementById('anuncio-container');
-    
-    // Verifica que el contenedor exista antes de mostrar el anuncio
-    if (container) {
-      // Muestra el anuncio en el contenedor
-      container.innerHTML = '<ins class="adsbygoogle" style="display:block" data-ad-format="auto"></ins>';
-      
-      // Carga el anuncio
-      (adsbygoogle = window.adsbygoogle || []).push({});
-    }
-  }
-  
-  // Llama a las funciones para configurar y mostrar el anuncio
-  configurarAnuncio();
-  mostrarAnuncio();
+
+
   
