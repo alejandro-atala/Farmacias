e81e2c273dd3be751c38ba9b7a5b33f534ecbae5
Alejandro <alejandro.atala@municipiodetandil.edu.ar>
2023-12-08 07:40:10 -0300

    datos

--- script.js
+++ script.js
@@ -1,54 +1,40 @@
-// // Definir un objeto vacío para almacenar las farmacias por localidad
-// const farmaciasPorLocalidad = {};
+function mostrarCartelDescarga() {
+  // Verifica si la cookie "appDescargada" está presente
+  const appDescargada = getCookie("appDescargada");
 
-// // Función para leer el archivo CSV y cargar los datos en el objeto farmaciasPorLocalidad
-// function cargarFarmaciasDesdeCSV() {
-//   Papa.parse("datos.csv", {
-//     download: true,
-//     complete: function (result) {
-//       // Iterar sobre las filas del archivo CSV (ignorando la primera fila de encabezados)
-//       for (let i = 1; i < result.data.length; i++) {
-//         const [localidad, nombre, direccion, telefono] = result.data[i];
-//         if (!farmaciasPorLocalidad[localidad]) {
-//           farmaciasPorLocalidad[localidad] = [];
-//         }
-//         farmaciasPorLocalidad[localidad].push({ nombre, direccion, telefono });
-//       }
+  // Si la cookie no está presente, muestra el cartel y establece la cookie
+  if (!appDescargada) {
+    const cartel = document.getElementById("downloadApp");
+    cartel.style.display = "block";
+    // Establece la cookie "appDescargada" con un valor de "true" (puede cambiar esto según tus necesidades)
+    setCookie("appDescargada", "true", 365); // La cookie expira en 365 días
+  }
+}
 
-//       // Una vez que se cargaron los datos, llamar a la función para mostrar la farmacia de turno actual
-//       mostrarFarmaciaDeTurno();
-//     },
-//   });
-// }
+// Función para establecer una cookie
+function setCookie(name, value, days) {
+  const date = new Date();
+  date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+  const expires = "expires=" + date.toUTCString();
+  document.cookie = name + "=" + value + ";" + expires + ";path=/";
+}
 
-// // Función para obtener la farmacia de turno actual
-// function obtenerFarmaciaDeTurno(localidad) {
-//   const farmacias = farmaciasPorLocalidad[localidad];
-//   const semanaActual = Math.floor(Date.now() / (7 * 24 * 60 * 60 * 1000)); // Semana actual en milisegundos
-//   const indiceFarmacia = semanaActual % farmacias.length;
-//   return farmacias[indiceFarmacia];
-// }
-
-// // Función para mostrar la farmacia de turno según la localidad seleccionada
-// function mostrarFarmaciaDeTurno() {
-//   const localidadSelect = document.getElementById("localidad");
-//   const localidadSeleccionada = localidadSelect.value;
-//   const farmaciaDeTurno = obtenerFarmaciaDeTurno(localidadSeleccionada);
-
-//   const farmaciaTurno = document.getElementById("farmaciaTurno");
-//   farmaciaTurno.innerHTML = `
-//     <h3>${farmaciaDeTurno.nombre}</h3>
-//     <p>${farmaciaDeTurno.direccion}</p>
-//     <p>${farmaciaDeTurno.telefono}</p>
-//     <hr>
-//   `;
-// }
-
-// // Asociar la función cargarFarmaciasDesdeCSV al evento de carga de la página
-// window.addEventListener("load", cargarFarmaciasDesdeCSV);
-
-// // Asociar la función mostrarFarmaciaDeTurno al evento de cambio del select
-// document.getElementById("localidad").addEventListener("change", mostrarFarmaciaDeTurno);
+// Función para obtener el valor de una cookie
+function getCookie(name) {
+  const cname = name + "=";
+  const decodedCookie = decodeURIComponent(document.cookie);
+  const cookieArray = decodedCookie.split(";");
+  for (let i = 0; i < cookieArray.length; i++) {
+    let c = cookieArray[i];
+    while (c.charAt(0) == " ") {
+      c = c.substring(1);
+    }
+    if (c.indexOf(cname) == 0) {
+      return c.substring(cname.length, c.length);
+    }
+  }
+  return "";
+}
 
 
 // Definir un objeto vacío para almacenar las farmacias por localidad
@@ -56,6 +42,7 @@ const farmaciasPorLocalidad = {};
 
 // Función para leer el archivo CSV y cargar los datos en el objeto farmaciasPorLocalidad
 function cargarFarmaciasDesdeCSV() {
+  document.querySelector(".progress-bar").style.display = "block";
   Papa.parse("datos.csv", {
     download: true,
     complete: function (result) {
@@ -67,32 +54,13 @@ function cargarFarmaciasDesdeCSV() {
         }
         farmaciasPorLocalidad[localidad].push({ nombre, direccion, telefono });
       }
-
+      setTimeout(function () {
+        document.querySelector(".progress-bar").style.display = "none";
       // Una vez que se cargaron los datos, llamar a la función para mostrar la farmacia de turno actual
       mostrarFarmaciaDeTurno();
+    }, 2000);
     },
   });
-}
-
-// Función para obtener la farmacia de turno actual considerando el día de la semana y hora actual
-function obtenerFarmaciaDeTurno(localidad) {
-  const farmacias = farmaciasPorLocalidad[localidad];
-  const now = new Date(); // Fecha y hora actual
-  const currentWeek = getWeekNumber(now); // Obtener el número de semana actual
-
-  // Obtener el número de semana almacenado en el almacenamiento local (si existe)
-  const lastWeek = parseInt(localStorage.getItem("lastWeek"), 10);
-
-  // Si es la misma semana que la almacenada, obtener la farmacia anterior
-  if (lastWeek && currentWeek === lastWeek) {
-    const indiceFarmacia = (currentWeek - 1) % farmacias.length;
-    return farmacias[indiceFarmacia];
-  }
-
-  // Si es una semana diferente, almacenar la nueva semana y obtener la siguiente farmacia
-  localStorage.setItem("lastWeek", currentWeek.toString());
-  const indiceFarmacia = currentWeek % farmacias.length;
-  return farmacias[indiceFarmacia];
 }
 
 // Función para obtener el número de semana a partir de una fecha
@@ -100,6 +68,48 @@ function getWeekNumber(date) {
   const onejan = new Date(date.getFullYear(), 0, 1);
   const millisecsInDay = 86400000;
   return Math.ceil(((date - onejan) / millisecsInDay + onejan.getDay() + 1) / 7);
+}
+
+// Función para obtener la próxima farmacia de la lista según la localidad y el día actual
+function obtenerFarmaciaDeTurno(localidad) {
+  const farmacias = farmaciasPorLocalidad[localidad];
+  const now = new Date(); // Fecha y hora actual
+  const dayOfWeek = now.getDay(); // Número del día de la semana (0 para domingo, 1 para lunes, etc.)
+  const hour = now.getHours(); // Hora actual
+  const minutes = now.getMinutes(); // Minutos actuales
+
+  if (localidad === "Dorrego") {
+    if (dayOfWeek === 4 && hour === 0 && minutes === 0) {
+      // Es jueves a las 00:00 horas, avanzar al siguiente viernes
+      const daysUntilNextFriday = (7 - dayOfWeek + 5) % 7;
+      now.setDate(now.getDate() + daysUntilNextFriday);
+    } else if (dayOfWeek !== 4) {
+      // No es jueves, avanzar al próximo jueves
+      const daysUntilNextThursday = (4 - dayOfWeek + 7) % 7;
+      now.setDate(now.getDate() + daysUntilNextThursday);
+    }
+
+    const daysSinceStart = Math.floor((now.getTime() - new Date('2023-01-01').getTime()) / (1000 * 60 * 60 * 24));
+    const weeksSinceStart = Math.floor(daysSinceStart / 7); // Calcular semanas desde el inicio
+    const indiceFarmacia = weeksSinceStart % farmacias.length;
+    return farmacias[indiceFarmacia];
+  } else if (localidad === "Monte") {
+    if (hour >= 9) {
+      // Calcular la diferencia en días entre la fecha actual y la fecha de inicio
+      const daysSinceStart = Math.floor((now.getTime() - new Date('2023-07-02').getTime()) / (1000 * 60 * 60 * 24));
+
+      // Calcular el índice basado en los días desde el inicio
+      const indiceFarmacia = daysSinceStart % farmacias.length;
+      console.log(indiceFarmacia);
+      return farmacias[indiceFarmacia];
+    } else {
+      // Antes de las 9 a. m., seleccionar la farmacia del día anterior
+      const daysSinceStart = Math.floor((now.getTime() - new Date('2023-07-02').getTime()) / (1000 * 60 * 60 * 24));
+      const indiceFarmaciaAyer = (daysSinceStart - 1 + farmacias.length) % farmacias.length;
+
+      return farmacias[indiceFarmaciaAyer];
+    }
+  }
 }
 
 // Función para mostrar la farmacia de turno según la localidad seleccionada
@@ -110,12 +120,22 @@ function mostrarFarmaciaDeTurno() {
 
   const farmaciaTurno = document.getElementById("farmaciaTurno");
   farmaciaTurno.innerHTML = `
-    <p>${farmaciaDeTurno.nombre}</p>
-    <p>${farmaciaDeTurno.direccion}</p>
-    <p>${farmaciaDeTurno.telefono}</p>
-    <hr>
+
+    <p><b><u>${farmaciaDeTurno.nombre}</u></b></p>
+    <p><img src="./img/Google-Maps-Logo.png" alt="Google Maps" class="google-maps-logo">
+    <a class="farmacia-direccion" href="https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(farmaciaDeTurno.direccion)}" target="_blank">${farmaciaDeTurno.direccion}</a></p>
+    <p><img src="./img/tel.png" alt="Google Maps" class="tel-logo">
+    <a class="farmacia-telefono" href="tel:${farmaciaDeTurno.telefono}">${farmaciaDeTurno.telefono}</a></p>
   `;
+  // Mostrar el párrafo una vez que los datos se han cargado
+  farmaciaTurno.querySelectorAll("p").forEach((paragraph) => {
+    paragraph.style.display = "block";
+  });
 }
+
+
+
+
 
 // Asociar la función cargarFarmaciasDesdeCSV al evento de carga de la página
 window.addEventListener("load", cargarFarmaciasDesdeCSV);
@@ -125,6 +145,3 @@ document.getElementById("localidad").addEventListener("change", mostrarFarmaciaD
 
 // Mostrar la farmacia de turno actual para la primera localidad al cargar la página
 mostrarFarmaciaDeTurno();
-
-
-
